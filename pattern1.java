9d3a9fcc2f43dfc68f9c89a0db901d3fb3c52dae
Prasannajeet <prasannajeet.2426mca@kiet.edu>
2026-01-20 11:24:00 +0530

    second commit

--- pattern1.java
+++ pattern1.java
@@ -1,5 +1,14 @@
 public class pattern1{
     public static void main(String[] args) {
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        
         System.out.println("*");
+        // System.out.println("Right aligned");
+        // for(int i=0; i<n; i++){
+        //     if(i==n-1) System.out.print("*");
+        //     else System.out.print("_");
+        // }
     }
 }
